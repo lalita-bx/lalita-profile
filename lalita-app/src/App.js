6bf681c6cf6ec811cfedf5317a2df92afa9950ab
LALITA YODSRI <lalita_yo@cmu.ac.th>
2022-08-27 23:43:18 +0700

    change bg color header

--- lalita-app/src/App.js
+++ lalita-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <p class="bg-red-500">
+                <p>
                     header.
                 </p>
                 {/* <img src={logo} className="App-logo" alt="logo" />
