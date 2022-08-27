2c53a29742ea11172d9387acad68d15590a1a4f2
LALITA YODSRI <lalita_yo@cmu.ac.th>
2022-08-27 23:40:28 +0700

    Create app and install tailwind'

---
+++ lalita-app/src/components/ProfileDetails.js
@@ -0,0 +1,11 @@
+import './ProfileDetails.css';
+
+const ProfileDetails = () =>{
+    return (
+        <div className="profile-body">
+            Be Happy
+        </div>
+    );
+}
+
+export default ProfileDetails
