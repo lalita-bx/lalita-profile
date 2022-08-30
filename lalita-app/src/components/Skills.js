2895d15f1f975ad8c3a8bda07ef6efec36b4141b
LALITA YODSRI <lalita_yo@cmu.ac.th>
2022-08-30 16:51:19 +0700

    skill details

--- lalita-app/src/components/Skills.js
+++ lalita-app/src/components/Skills.js
@@ -1,16 +1,16 @@
 const Skills = () =>{
     return (
-        <div class="grid grid-cols-7 gap-4 p-16">
-            <div class=" bg-black pt-10"><img class="w-full" src="/logo192.png" alt="react"></img></div>
-            <div class="col-span-2 bg-slate-300"><img src="/logo192.png" alt="react"></img></div>
-            <div class="bg-black"><img src="/logo192.png" alt="react"></img></div>
-            <div class="col-span-2 bg-slate-300"><img class="relative h-32 w-32" src="/logo192.png" alt="react"></img></div>
-            <div class="bg-black"><img src="/logo192.png" alt="react"></img></div>
-            <div class="bg-slate-300"><img src="/logo192.png" alt="react"></img></div>
-            <div class=" bg-black"><img class="object-center" src="/logo192.png" alt="react"></img></div>
-            
+        <div>
+            <div class="flex flex-wrap flex-col items-center justify-center">
+                <div class="col-span-3 justify-center"><img class="h-48 mt-10 hover:scale-110 transition duration-300 ease-in-out" src="/logo-basic.png" alt="basic frontend"></img></div>
+            </div>
+            <div class="grid grid-cols-3 gap-4">
+                <div class="self-end justify-self-end"><img src="/logo-salesforce.png" alt="salesforce" className="h-40 hover:scale-110 transition duration-300 ease-in-out"></img></div>
+                <div class="self-center justify-self-center"><img src="/logo512.png" alt="react" className="h-44 hover:scale-110 transition duration-300 ease-in-out"></img></div>
+                <div class="self-end justify-self-start"><img src="/logo-vue.png" alt="vue" className="h-40 hover:scale-110 transition duration-300 ease-in-out"></img></div>
+                <div class="self-center justify-self-center col-span-3"><img src="/logo-python.png" alt="python" className="h-40 hover:scale-110 transition duration-300 ease-in-out"></img></div>
+            </div>
         </div>
-
     );
 }
 
