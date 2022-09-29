9dc5e3eaaf4e935822f721ab991886a3d66efa07
LALITA YODSRI <lalita_yo@cmu.ac.th>
2022-09-30 00:54:49 +0700

    update indentation and etc.

--- lalita-app/src/components/Contact.js
+++ lalita-app/src/components/Contact.js
@@ -1,16 +1,16 @@
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import { 
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import {
     IconButton,
-    Tooltip, 
+    Tooltip,
 } from "@material-tailwind/react";
 
-const Contact = () =>{
-    
+const Contact = () => {
+
     return (
         <div className="grid place-content-center h-96 pt-14 md:pt-40 text-justify text-base md:text-2xl overflow-y-auto">
             <div>
-                <i className="pr-4 fas fa-envelope" /> 
-                <p className="inline-block text-sm md:text-2xl">lalita.yodsri@gmail.com</p> 
+                <i className="pr-4 fas fa-envelope" />
+                <p className="inline-block text-sm md:text-2xl">lalita.yodsri@gmail.com</p>
                 <CopyToClipboard text='lalita.yodsri@gmail.com'
                     onCopy={() => console.log('copied')}>
                     <IconButton variant="text" size="sm" className='pt-3'>
@@ -21,7 +21,7 @@ const Contact = () =>{
                 </CopyToClipboard>
             </div>
             <div><i className="pl-0.5 pr-3.5 fas fa-mobile-alt" /> 065-347-0896</div>
-            <div><i className="pl-0.5 pr-3.5 fas fa-id-badge" /> My <a href="https://drive.google.com/file/d/1qiMjVNmfqAcZxVXf3sEj-5c0fRoff8_H/view?usp=sharing" target="blank" className="font-bold text-deep-purple-400 hover:text-deep-purple-900">resume <i className="fas fa-external-link-alt fa-xs" /></a></div>
+            <div><i className="pl-0.5 pr-3.5 fas fa-id-badge" /> My <a href="https://drive.google.com/file/d/1VT_rgZa_wD1f9DOw_6QvaBWZfJaUCFd9/view?usp=sharing" target="blank" className="font-bold text-deep-purple-400 hover:text-deep-purple-900">resume <i className="fas fa-external-link-alt fa-xs" /></a></div>
 
         </div>
     );
--- lalita-app/src/components/Skills.js
+++ lalita-app/src/components/Skills.js
@@ -1,51 +1,51 @@
-import { 
-    Tooltip, 
+import {
+    Tooltip,
 } from "@material-tailwind/react";
 
-const Skills = () =>{
+const Skills = () => {
     return (
         <div>
             <div className="grid grid-cols-3 grid-flow-row py-5">
                 <div className="self-center justify-self-center col-span-3 pt-16 md:pt-10">
                     <Tooltip content="Basic Frontend" placement="left-start">
-                        <img className="block mx-auto h-36 hover:scale-110 transition duration-300 ease-in-out" src="/logo-basic.png" alt="basic frontend"/>
+                        <img className="block mx-auto h-36 hover:scale-110 transition duration-300 ease-in-out" src="/logo-basic.png" alt="basic frontend" />
                     </Tooltip>
                 </div>
-       
+
                 <div className="self-end justify-self-end">
                     <Tooltip content="Salesforce" placement="left-end">
-                        <img src="/logo-salesforce.png" alt="salesforce" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out"/>
+                        <img src="/logo-salesforce.png" alt="salesforce" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out" />
                     </Tooltip>
                 </div>
 
                 <div className="self-center justify-self-center">
                     <Tooltip content="React" placement="right-end">
-                        <img src="/logo-react.png" alt="react" className="inline-block max-h-36 m-auto hover:scale-110 transition duration-300 ease-in-out"/>
+                        <img src="/logo-react.png" alt="react" className="inline-block max-h-36 m-auto hover:scale-110 transition duration-300 ease-in-out" />
                     </Tooltip>
                 </div>
 
                 <div className="self-end justify-self-start">
                     <Tooltip content="Python" placement="right-end">
-                        <img src="/logo-python.png" alt="python" className="inline-block m-auto max-h-20 md:max-h-28 hover:scale-110 transition duration-300 ease-in-out"/>
+                        <img src="/logo-python.png" alt="python" className="inline-block m-auto max-h-20 md:max-h-28 hover:scale-110 transition duration-300 ease-in-out" />
                     </Tooltip>
                 </div>
 
                 <div className="grid grid-cols-5 col-span-3 pt-5">
                     <div className="self-end justify-self-end col-start-2">
                         <Tooltip content="Github" placement="bottom">
-                            <img src="/logo-github.png" alt="github" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out"/>
+                            <img src="/logo-github.png" alt="github" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out" />
                         </Tooltip>
                     </div>
 
                     <div className="self-center justify-self-center">
                         <Tooltip content="Gitlab" placement="bottom">
-                            <img src="/logo-gitlab.png" alt="gitlab" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out"/>
+                            <img src="/logo-gitlab.png" alt="gitlab" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out" />
                         </Tooltip>
                     </div>
 
                     <div className="self-end justify-self-start">
                         <Tooltip content="SourceTree" placement="bottom">
-                            <img src="/logo-sourcetree.png" alt="sourcetree" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out"/>
+                            <img src="/logo-sourcetree.png" alt="sourcetree" className="inline-block m-auto max-h-28 hover:scale-110 transition duration-300 ease-in-out" />
                         </Tooltip>
                     </div>
                 </div>
@@ -55,7 +55,7 @@ const Skills = () =>{
                         .etc
                     </Tooltip>
                 </div>
-             
+
             </div>
         </div>
     );
