a01fb8bf46a9098b4a901c0009b3711f79b7473d
LALITA YODSRI <lalita_yo@cmu.ac.th>
2022-10-07 09:58:43 +0700

    change solution for dynamic className'

--- lalita-app/src/components/AnimeQuotes.js
+++ lalita-app/src/components/AnimeQuotes.js
@@ -6,20 +6,14 @@ const QUOTES = "_quotes";
 
 const AnimeQuotes = () => {
     const [quotesData, setQuotesData] = useState();
-    const [index, setIndex] = useState(1);
-    const [previousIndex, setPreviousIndex] = useState(0);
+    const [index, setIndex] = useState(0);
+    const [previousIndex, setPreviousIndex] = useState(undefined);
 
     useEffect(() => {
         const getAnimeQuotesService = async () => {
             let quotes = await GlobalService.getAnimeQuotes();
             if (quotes.status === 200) {
                 setQuotesData(quotes.data);
-                setTimeout(() => {
-                    const newElement = document.getElementById(0 + QUOTES);
-                    newElement.className = "cursor-pointer text-focus-in block";
-                    const nextPageElement = document.getElementById('next-page');
-                    nextPageElement.className = "h-0 bottom-0 w-32 ";
-                }, 2);
             } else {
                 console.log(quotes);
             }
@@ -27,18 +21,10 @@ const AnimeQuotes = () => {
         getAnimeQuotesService();
     }, []);
 
-    const setDisplay = () => {
-        const oldElement = document.getElementById(previousIndex + QUOTES);
-        const newElement = document.getElementById(index + QUOTES);
-        oldElement.className = "text-blur-out h-0 opacity-0";
-        newElement.className = "cursor-pointer text-focus-in delay-150 block";
-    };
-
     const nextQuotes = () => {
         setPreviousIndex(index);
         let dataSize = quotesData?.length || 1;
         setIndex((index + 1) % dataSize);
-        setDisplay();
     }
 
     useEffect(() => {
@@ -52,12 +38,22 @@ const AnimeQuotes = () => {
         nextQuotes();
     }
 
+    const getQuotesClassName = (i) => {
+        if (i === index) {
+            const delayClass = i !== 0 ? " delay-150" : "";
+            return "cursor-pointer text-focus-in block" + delayClass;
+        } else if (i === previousIndex) {
+            return "text-blur-out h-0 opacity-0";
+        }
+        return "hidden";
+    }
+
     return (
         <div>
             {quotesData
                 ? quotesData.map(({ quote, character, anime }, i) => (
-                    <div>
-                        <div id={i + QUOTES} key={i} onClick={skipQuotes} className="hidden">
+                    <>
+                        <div id={i + QUOTES} key={i} onClick={skipQuotes} className={getQuotesClassName(i)}>
                             <div className="lg:px-60 lg:text-3xl px-10 font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-300  via-deep-purple-200 to-pink-200 animate-text ">
                                 <i className="fas fa-quote-left fa-sm px-3" />
                                 {quote}
@@ -71,14 +67,14 @@ const AnimeQuotes = () => {
                                 <br />
                             </div>
                         </div>
-                        <div id="next-page" className="hidden">
+                        <div id="next-page" className="h-0 bottom-0 w-32">
                             <div className="absolute inset-x-0 lg:bottom-10 bottom-2 h-16 text-focus-in">
                                 <a href="#section2" className="text-deep-purple-200 ">
                                     <i className="fas fa-angle-down mb-10 fa-2x animate-bounce w-6 h-6" />
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </>
                 ))
                 :
                 <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
